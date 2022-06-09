0fd661f580c932fab74c155c7456e1d68ef5f947
Guilherme Schmitt <guilherme.schmitt@alkasoft.com.br>
2022-06-09 15:06:02 -0300

    Buscando os dados via API na home e exibindo-os de forma simples

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,48 @@
+import Image from 'next/image';
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export async function getStaticProps() {
+  const limit = 151;
+  const api = 'https://pokeapi.co/api/v2/pokemon/';
+
+  const res = await fetch(`${api}?limit=${limit}`);
+  const data = await res.json();
+
+  data.results.forEach((item, index) => {
+    item.id = index + 1;
+  });
+
+  return {
+    props: {
+      pokemons: data.results
+    }
+  }
+}
+
+export default function Home({ pokemons }) {
   return (
-    <div>
-      <h1>
-        TODO:
-      </h1>
-    </div>
-  
+    <>
+      <div className={styles.title_container}>
+        <h1 className={styles.title}>
+          Next - Poke<span>API</span>
+        </h1>
+        <Image
+          width='50'
+          height='50'
+          alt='Pokeball'
+          src='/images/pokeball.png'
+        />
+      </div>
+      <div className={styles.pokemon_container}>
+        {
+          pokemons.map(pokemon => (
+            <p key={pokemon.id}>
+              {pokemon.name}
+            </p>
+          ))
+        }
+      </div>
+    </>
+
   )
 }
